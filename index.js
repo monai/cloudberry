caf54ac683eaa4ee97bfa9b57ecf9d967d45a572
monai <monai@cure.lt>
2016-08-06 11:00:13 +0300

    Cleanup

--- index.js
+++ index.js
@@ -1,85 +1,12 @@
 const proxy = require('./lib/proxy');
+const connect = require('connect');
+const morgan = require('morgan');
 
-// const fs = require('fs');
-// const net = require('net');
-// const url = require('url');
-// const http = require('http');
-// const https = require('https');
-//
-// var conns = {};
-//
-// const options = {
-//   host: '127.0.0.1',
-//   port: 8000
-// };
-//
-// const proxy = http.createServer((req, res) => {
-//
-//   var opts = url.parse(req.url);
-//   opts.headers = req.headers;
-//
-//   const target = http.request(opts, (tRes) => {
-//     res.writeHead(tRes.statusCode, tRes.statusMessage, tRes.headers);
-//     tRes.pipe(res);
-//   });
-//
-//   req.pipe(target);
-// });
-//
-// proxy.listen(options.port, options.host, () => {
-//   console.log('Proxy is listening on %s:%s', options.host, options.port);
-// });
-//
-// const imOptions = {
-//   host: '127.0.0.1',
-//   port: 8001,
-// };
-//
-// const httpsOptions = {
-//   key: fs.readFileSync('key.pem'),
-//   cert: fs.readFileSync('cert.pem')
-// };
-//
-// const im = https.createServer(httpsOptions, (req, res) => {
-//   var opts = url.parse(req.url);
-//   Object.assign(opts, conns[req.socket.remotePort]);
-//   opts.headers = req.headers;
-//
-//   const target = https.request(opts, (tRes) => {
-//     res.writeHead(tRes.statusCode, tRes.statusMessage, tRes.headers);
-//     tRes.pipe(res);
-//   });
-//
-//   req.pipe(target);
-// });
-//
-// im.listen(imOptions.port, imOptions.host, () => {
-//   console.log('Intermediate is listening on %s:%s', imOptions.host, imOptions.port);
-// });
-//
-// proxy.on('connect', (req, client, header) => {
-//   const resHeader = [
-//     'HTTP/1.1 200 Connection Established',
-//     'proxy-agent: proxy',
-//     '', ''
-//   ].join('\r\n');
-//
-//   const [host, port] = req.url.split(':');
-//   const protocol = port == 443 ? 'https:' : 'http:';
-//   const reqUrl = { protocol, host, port };
-//
-//   client.write(resHeader);
-//
-//   const target = net.connect(imOptions);
-//   target.write(header);
-//   target.pipe(client);
-//   client.pipe(target);
-//
-//   target.on('connect', () => {
-//     conns[target.localPort] = reqUrl;
-//   })
-//
-//   client.on('end', () => {
-//     delete conns[target.localPort];
-//   });
-// });
+var app = connect();
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  console.log(res.statusCode, res.statusMessage);
+  next();
+});
+
+proxy({}, app);
