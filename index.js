a42b55c02e76d363091d72737474d311f09f75c2
monai <monai@cure.lt>
2021-01-26 10:23:16 +0200

    Update exports

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const CertificateAuthority = require('./lib/CertificateAuthority');
-const createServer = require('./lib/server');
+const createServer = require('./lib/createServer');
+const lookupCertificate = require('./lib/lookupCertificate');
 const proxy = require('./lib/proxy');
 const request = require('./lib/request');
 
 module.exports = {
   CertificateAuthority,
   createServer,
+  lookupCertificate,
   proxy,
   request,
 };
